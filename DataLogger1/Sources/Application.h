341a2aec9522b97267901a0aa9132ce4665eb0c3
fnqbruce <bruce.belson@my.jcu.edu.au>
2018-12-02 12:30:24 +1000

    First DataLogger build
    After following all steps at https://mcuoneclipse.com/2014/05/26/tutorial-data-logger-with-the-frdm-k64f-board/

---
+++ DataLogger1/Sources/Application.h
@@ -0,0 +1,13 @@
+/*
+ * Application.h
+ *
+ *  Created on: 2 Dec 2018
+ *      Author: Bruce Belson
+ */
+
+#ifndef SOURCES_APPLICATION_H_
+#define SOURCES_APPLICATION_H_
+
+void APP_Run(void);
+
+#endif /* SOURCES_APPLICATION_H_ */
